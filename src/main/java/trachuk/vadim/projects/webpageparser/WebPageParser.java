dcbc9409fb34d9a9a46850aed89266b2e84a3ef3
VadikT <VadikT@users.noreply.github.com>
2020-02-11 21:49:32 +0200

    added a getIpAdrress method

--- src/main/java/trachuk/vadim/projects/webpageparser/WebPageParser.java
+++ src/main/java/trachuk/vadim/projects/webpageparser/WebPageParser.java
@@ -1,10 +1,7 @@
 package trachuk.vadim.projects.webpageparser;
 
 import java.io.*;
-import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -13,21 +10,23 @@ import java.util.regex.Pattern;
 public class WebPageParser {
     private final String url;
     public static final int MAX_ARGS_LENGTH = 1;
-    private final String URL_PATTERN = "(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)?";
+    //private final String URL_PATTERN = "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$";
+    private final String URL_PATTERN = "(@)?(href=')?(HREF=')?(HREF=\")?(href=\")?(http://)?(https://)?[a-zA-Z_0-9\\-]+(\\.\\w[a-zA-Z_0-9\\-]+)+(/[#&\\n\\-=?\\+\\%/\\.\\w]+)?";
 
-
+//main method
     public static void main(String[] args) throws Exception, RuntimeException {
         if (args != null && args.length == MAX_ARGS_LENGTH) {
 
             WebPageParser webPageParser = null;
             webPageParser = new WebPageParser(args[0]);
-            try {
+            try { webPageParser.isUrlMatch();
                 webPageParser.parse();
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
             webPageParser.getIpAdrress();
+            //webPageParser.isPortAwailible(url, 80);
         } else {
             System.out.println("wrong number of param. max valiable args count is " + MAX_ARGS_LENGTH);
         }
@@ -36,20 +35,25 @@ public class WebPageParser {
 
     public WebPageParser(String url) throws Exception {
         this.url = url;
-        isUrlMatch();
+       // isUrlMatch();
+       // getIpAdrress();
+       // isPortAwailible(url, 80);
         //перенасти в отдельный метод+
         // проверить с regexp что это именно 1 url и выкинуть ексепшн если не url if+-
     }
 
+
+    //method that validates URL
     public boolean isUrlMatch() throws WrongUrlParamException {
         Pattern pattern = Pattern.compile(URL_PATTERN);
         Matcher matcher = pattern.matcher(url);
-        if (matcher.matches() == true) {
-            return matcher.matches();
-        } else {
-            return false;
-        }
+        if (matcher.matches() == true){
+            System.out.println("URL is correct");
+        }else if (matcher.matches() == false){
+            System.out.println("URL is not correct");
 
+        }
+        return true;
     }
 
 
@@ -90,7 +94,7 @@ public class WebPageParser {
 
         return list;
     }
-
+        //method witch cheks ip address by host
 /*
     public String getIpAdrress(int port) throws UnknownHostException {
 
@@ -107,12 +111,55 @@ public class WebPageParser {
 
      return getAllIp;
     }*/
+
+
+//method witch cheks ip address by host
         public String getIpAdrress() throws IOException {
             InetAddress ip = InetAddress.getByName(new URL(this.url)
                     .getHost());
             System.out.println("Public IP Address of: " + ip);
+
             return String.valueOf(ip);
         }
+
+    //method witch cheks port
+    public static boolean isPortAwailible(String hostName, int port) {
+
+        boolean isAlive = false;
+
+        // Creates a socket address from a hostname and a port number
+        SocketAddress socketAddress = new InetSocketAddress(hostName, port);
+        Socket socket = new Socket();
+
+        // Timeout required - it's in milliseconds
+        int timeout = 2000;
+
+        log("hostName: " + hostName + ", port: " + port);
+        try {
+            socket.connect(socketAddress, timeout);
+            socket.close();
+            isAlive = true;
+
+        } catch (SocketTimeoutException exception) {
+            System.out.println("SocketTimeoutException " + hostName + ":" + port + ". " + exception.getMessage());
+        } catch (IOException exception) {
+            System.out.println(
+                    "IOException - Unable to connect to " + hostName + ":" + port + ". " + exception.getMessage());
+        }
+        System.out.println(isAlive);
+        return isAlive;
+    }
+
+    // Simple log utility
+    private static void log(String string) {
+        System.out.println(string);
+    }
+
+    // Simple log utility returns boolean result
+    private static void log(boolean isAlive) {
+        System.out.println("isAlive result: " + isAlive + "\n");
+    }
+
 
 }
 
--- src/test/java/trachuk/vadim/projects/webpageparser/WebPageParserTest.java
+++ src/test/java/trachuk/vadim/projects/webpageparser/WebPageParserTest.java
@@ -3,10 +3,33 @@ package trachuk.vadim.projects.webpageparser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WebPageParserTest {
+
+    @Test
+    public void tester() throws Exception {
+        String url = "https://www.you tube.com";
+        WebPageParser WebPageParser = new WebPageParser(url);
+        if (WebPageParser.equals("URL is correct")) {
+            Assertions.assertTrue(true);
+        } else if (WebPageParser.equals("URL is not correct")){
+            Assertions.fail("not correct URL");
+
+        }
+
+    }
+    @Test
+    public void tester2() throws Exception{
+        String url = "https://www.you tube.com";
+        WebPageParser webPageParser = new WebPageParser(url);
+        if (webPageParser.isUrlMatch()){
+            Assertions.fail("nop");
+        }
+    }
+
 
     @Test
     public void testRightHttp() throws Exception {
@@ -16,33 +39,25 @@ public class WebPageParserTest {
         Assertions.assertTrue(true);
     }//создать 2 правильных теста и 2 неправильных
 
-    //как на junit 5 создать входные параметры для тестового метода с набором данных
+
     @Test
     public void testRightHttps() throws Exception {
         String url = "https://www.youtube.com/";
         WebPageParser webPageParser = new WebPageParser(url);
-        webPageParser.isUrlMatch();
-        Assertions.assertTrue(true);
+        if (webPageParser.isUrlMatch() == false) {
+            Assertions.assertTrue(true);
+            System.out.println("ok");
+        } else {
+            Assertions.fail("wrong url");
+        }
+
+
     }
 
     @Test
     public void testWrongHttp() throws Exception {
 
-        String url = "https://www.youtube.com/https://www.youtube.com/";
-        WebPageParser webPageParser = new WebPageParser(url);
-        if (webPageParser.isUrlMatch() == false) {
-            Assertions.fail();
-        } else {
-            System.out.println("ok");
-        }
-
-
-    }
-
-    @Test
-    public void testWrongHttps() throws Exception {
-
-        String url = "https://ww.you tube.com/";
+        String url = "https://www.youtube.com";
         WebPageParser webPageParser = new WebPageParser(url);
         if (webPageParser.isUrlMatch() == false) {
             Assertions.fail();
@@ -51,35 +66,38 @@ public class WebPageParserTest {
         }
     }
 
-    @Test
-    public void testQweQweFail() {
-        String testData = "qweqwe";
 
-        Pattern pattern = Pattern.compile("(.+)\\1+");                 //("(\\p{L})(?=.*\\1++)");
-        Matcher matcher = pattern.matcher(testData);
-        if (matcher.find()){
+        @Test
+        public void testQweQweFail () {
+            String testData = "qweqwe";
 
-            System.out.println("найдено совпадение" + matcher.group());
-        }else {
-            System.out.println("ok");
-        }
-        //System.out.println(matcher.find()) ;
+            Pattern pattern = Pattern.compile("(.+)\\1+");                 //("(\\p{L})(?=.*\\1++)");
+            Matcher matcher = pattern.matcher(testData);
+            if (matcher.find()) {
 
-    }
+                System.out.println("найдено совпадение" + matcher.group());
 
-    @Test
-    public void testQtySuccess() {
-        String testData = "qwe";
+            } else {
+                System.out.println("ok");
+            }
 
-        Pattern pattern = Pattern.compile("(.+)\\1+");                 //("(\\p{L})(?=.*\\1++)");
-        Matcher matcher = pattern.matcher(testData);
-        if (matcher.find()){
 
-            System.out.println("найдено совпадение" + matcher.group());
-        }else {
-            System.out.println("ok");
         }
 
+        @Test
+        public void testQtySuccess () {
+            String testData = "qwe";
+
+            Pattern pattern = Pattern.compile("(.+)\\1+");                 //("(\\p{L})(?=.*\\1++)");
+            Matcher matcher = pattern.matcher(testData);
+            if (matcher.find()) {
+
+                System.out.println("найдено совпадение" + matcher.group());
+            } else {
+                System.out.println("ok");
+            }
+
+        }
     }
-}
+
 
